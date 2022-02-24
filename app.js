077c68b0dbc2144816384ede488f6d0840dbad76
vikas pundir <vikaspundiruser@gmai.com>
2022-02-24 19:43:26 +0530

    API WORKING FINE

--- app.js
+++ app.js
@@ -3,7 +3,9 @@
 /* let apiKey = 1f3aa04f979248f8aedc6a19610dfac8; */
 
 // Grab the news container
-let container1  = document.querySelector('.container1');
+let slides1 = document.getElementById("slides1");
+let next = document.querySelector(".carousel-control-next");
+let back = document.querySelector(".carousel-control-prev")
 
 // Create an ajax get request
 const xhr1 = new XMLHttpRequest();
@@ -16,109 +18,158 @@ xhr1.onload = function () {
         let articles = json.articles;
         console.log(articles);
         let newsHtml = "";
-        articles.forEach(function(element, index) {
+
+
+
+        articles.forEach(function (element, index) {
             // console.log(element, index)
+
             let news = `
-            <img src="${element["urlToImage"]}" alt="" id="image1">
-            <h4>${element["title"]}</h4>
-            <p>${element["content"]}</p>
-            <h6>BUSINESS</h6>`;
-            newsHtml = news;
+
+
+                <div class="carousel-item " id="slide${index}">
+                    <img src="${element["urlToImage"]}" alt="" id="image1">
+                    <h4>${element["title"]}</h4>
+                    <p>${element["description"]}</p>
+                    <a href="${element["url"]}">READ MORE</a>
+                    
+                    <h6>BUSINESS</h6>
+                </div>
+               `;
+            newsHtml += news;
+
+
+
+
         });
-        container1.innerHTML = newsHtml;
-    }
-    else {
+        slides1.innerHTML = newsHtml;
+
+
+
+
+    } else {
         console.log("Some error occured")
     }
+    let x = 0;
+    const slideL = document.getElementById(`slide${x}`);
+    slideL.classList.add("active");
+
+    next.addEventListener("click", () => {
+        document.getElementById(`slide${x+1}`).classList.add("active");
+        document.getElementById(`slide${x}`).classList.remove("active");
+        x = x + 1;
+        return x;
+    })
+
+    back.addEventListener("click", () => {
+        document.getElementById(`slide${x-1}`).classList.add("active");
+        document.getElementById(`slide${x}`).classList.remove("active");
+        x = x - 1;
+        return x;
+    })
+
+
 }
+
 
 xhr1.send()
 
 
-/* ==== NEWS 2====== */
+/* ========================================================================================================== */
 
+
+/* ==== NEWS 2 ====== */
 
 /* let apiKey = 1f3aa04f979248f8aedc6a19610dfac8; */
 
 // Grab the news container
-let container21  = document.querySelector('.container21');
+let slides2 = document.getElementById("slides2");
+let nextAB = document.querySelector(".AB");
+let backBA = document.querySelector(".BA")
 
 // Create an ajax get request
-const xhr21 = new XMLHttpRequest();
-xhr21.open('GET', `https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=1f3aa04f979248f8aedc6a19610dfac8
+const xhr2 = new XMLHttpRequest();
+xhr2.open('GET', `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=1f3aa04f979248f8aedc6a19610dfac8
 `, true);
 
 // What to do when response is ready
-xhr21.onload = function () {
+xhr2.onload = function () {
     if (this.status === 200) {
         let json = JSON.parse(this.responseText);
         let articles = json.articles;
         console.log(articles);
         let newsHtml = "";
-        articles.forEach(function(element, index) {
+
+
+
+        articles.forEach(function (element, index) {
             // console.log(element, index)
-            let news = `<h4>${element["title"]}</h4>
-            <p>${element["content"]}</p>
-            <h6>ENTERTAINMENT</h6>`;
-            newsHtml = news;
+
+            let news = `
+
+
+                <div class="carousel-item " id="slideA${index}">
+                    <h4>${element["title"]}</h4>
+                    <p>${element["description"]}</p>
+                    <a href="${element["url"]}">READ MORE</a>
+                    
+                    <h6>SPORTS</h6>
+                </div>
+               `;
+            newsHtml += news;
+
+
+
+
         });
-        container21.innerHTML = newsHtml;
-    }
-    else {
+        slides2.innerHTML = newsHtml;
+
+
+
+
+    } else {
         console.log("Some error occured")
     }
+    let a = 0;
+    const slideLa = document.getElementById(`slideA${a}`);
+    slideLa.classList.add("active");
+
+    nextAB.addEventListener("click", () => {
+        document.getElementById(`slideA${a+1}`).classList.add("active");
+        document.getElementById(`slideA${a}`).classList.remove("active");
+        a = a + 1;
+        return a;
+    })
+
+    backBA.addEventListener("click", () => {
+        document.getElementById(`slideA${a-1}`).classList.add("active");
+        document.getElementById(`slideA${a}`).classList.remove("active");
+        a = a - 1;
+        return a;
+    })
+
+
 }
 
-xhr21.send()
+
+xhr2.send()
+
+/* ============================================================================================================ */
 
 
-/* ==== NEWS 3====== */
+
+/* ==== NEWS 3 ====== */
 
 /* let apiKey = 1f3aa04f979248f8aedc6a19610dfac8; */
 
 // Grab the news container
-let container22  = document.querySelector('.container22');
-
-// Create an ajax get request
-const xhr22 = new XMLHttpRequest();
-xhr22.open('GET', `https://newsapi.org/v2/top-headlines?country=in&category=sports&apiKey=1f3aa04f979248f8aedc6a19610dfac8
-`, true);
-
-// What to do when response is ready
-xhr22.onload = function () {
-    if (this.status === 200) {
-        let json = JSON.parse(this.responseText);
-        let articles = json.articles;
-        console.log(articles);
-        let newsHtml = "";
-        articles.forEach(function(element, index) {
-            // console.log(element, index)
-            let news = `<h4>${element["title"]}</h4>
-            <p>${element["content"]}</p>
-            <h6>ENTERTAINMENT</h6>`;
-            newsHtml = news;
-        });
-        container22.innerHTML = newsHtml;
-    }
-    else {
-        console.log("Some error occured")
-    }
-}
-
-xhr22.send()
-
-
-
-/* ==== NEWS 4====== */
-
-/* let apiKey = 1f3aa04f979248f8aedc6a19610dfac8; */
-
-// Grab the news container
-let container3  = document.querySelector('.container3');
+let slides3 = document.getElementById("slides3");
+let nextAC = document.querySelector(".AC");
+let backCA = document.querySelector(".CA")
 
 // Create an ajax get request
 const xhr3 = new XMLHttpRequest();
-xhr3.open('GET', `https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=1f3aa04f979248f8aedc6a19610dfac8`, true);
+xhr3.open('GET', `https://newsapi.org/v2/top-headlines?country=in&category=entertainment&apiKey=1f3aa04f979248f8aedc6a19610dfac8`, true);
 
 // What to do when response is ready
 xhr3.onload = function () {
@@ -127,20 +178,137 @@ xhr3.onload = function () {
         let articles = json.articles;
         console.log(articles);
         let newsHtml = "";
-        articles.forEach(function(element, index) {
+
+
+
+        articles.forEach(function (element, index) {
             // console.log(element, index)
+
             let news = `
-            <img src="${element["urlToImage"]}" alt="" id="image1">
-            <h4>${element["title"]}</h4>
-            <p>${element["content"]}</p>
-            <h6>HEALTH</h6>`;
-            newsHtml = news;
+
+
+                <div class="carousel-item " id="slideB${index}">
+                    <h4>${element["title"]}</h4>
+                    <p>${element["description"]}</p>
+                    <a href="${element["url"]}">READ MORE</a>
+                    
+                    <h6>ENTERTAINMENT</h6>
+                </div>
+               `;
+            newsHtml += news;
+
+
+
+
         });
-        container3.innerHTML = newsHtml;
-    }
-    else {
+        slides3.innerHTML = newsHtml;
+
+
+
+
+    } else {
         console.log("Some error occured")
     }
+    let c = 0;
+    const slideLb = document.getElementById(`slideB${c}`);
+    slideLb.classList.add("active");
+
+    nextAC.addEventListener("click", () => {
+        document.getElementById(`slideB${c+1}`).classList.add("active");
+        document.getElementById(`slideB${c}`).classList.remove("active");
+        c = c + 1;
+        return c;
+    })
+
+    backCA.addEventListener("click", () => {
+        document.getElementById(`slideB${c-1}`).classList.add("active");
+        document.getElementById(`slideB${c}`).classList.remove("active");
+        c = c - 1;
+        return c;
+    })
+
+
 }
 
+
 xhr3.send()
+
+/* ======================================================================================================= */
+
+/* ==== NEWS 4 ====== */
+
+/* let apiKey = 1f3aa04f979248f8aedc6a19610dfac8; */
+
+// Grab the news container
+let slides4 = document.getElementById("slides4");
+let nextAD = document.querySelector(".AD");
+let backDA = document.querySelector(".DA")
+
+// Create an ajax get request
+const xhr4 = new XMLHttpRequest();
+xhr4.open('GET', `https://newsapi.org/v2/top-headlines?country=in&category=health&apiKey=1f3aa04f979248f8aedc6a19610dfac8`, true);
+
+// What to do when response is ready
+xhr4.onload = function () {
+    if (this.status === 200) {
+        let json = JSON.parse(this.responseText);
+        let articles = json.articles;
+        console.log(articles);
+        let newsHtml = "";
+
+
+
+        articles.forEach(function (element, index) {
+            // console.log(element, index)
+
+            let news = `
+
+
+                <div class="carousel-item " id="slideC${index}">
+                    <img src="${element["urlToImage"]}" alt="" id="image1">
+                    <h4>${element["title"]}</h4>
+                    <p>${element["description"]}</p>
+                    <a href="${element["url"]}">READ MORE</a>
+                    
+                    <h6>HEALTH</h6>
+                </div>
+               `;
+            newsHtml += news;
+
+
+
+
+        });
+        slides4.innerHTML = newsHtml;
+
+
+
+
+    } else {
+        console.log("Some error occured")
+    }
+    let d = 0;
+    const slideLC = document.getElementById(`slideC${d}`);
+    slideLC.classList.add("active");
+
+    nextAD.addEventListener("click", () => {
+        document.getElementById(`slideC${d+1}`).classList.add("active");
+        document.getElementById(`slideC${d}`).classList.remove("active");
+        d = d + 1;
+        return d;
+    })
+
+    backDA.addEventListener("click", () => {
+        document.getElementById(`slideC${d-1}`).classList.add("active");
+        document.getElementById(`slideC${d}`).classList.remove("active");
+        d = d - 1;
+        return d;
+    })
+
+
+}
+
+
+xhr4.send()
+
+
